18ad20f54234d5d1db608078f013aad2d0dd0579
Joe Fiorini <joe@joefiorini.com>
2012-04-20 00:53:38 -0400

    Set/Unset active navigation item

--- test/bind-navigation-test.js
+++ test/bind-navigation-test.js
@@ -3,13 +3,18 @@ require("../lib/bind-navigation")
 
 module.exports = {
   "bindNavigation": {
-    beforeAll: function(){
+    beforeEach: function(){
       this.$container = $("<div>", { id: "test-container" })
       this.$link = $("<a class='nav-button' href='#'></a>")
 
-      this.router = { start: function(){ } }
+      this.router = { start: function(){ }, routeTo: function(){ } }
 
       this.$container.append(this.$link).appendTo("body")
+    },
+    afterEach: function(){
+      delete this.$container
+      delete this.$link
+      delete this.router
     },
     "binds to click on specified links": function(){
       var linkSpy = sinon.spy()
@@ -21,20 +26,39 @@ module.exports = {
       linkSpy.calledOnce.should.be.true
     },
     "binds to specific URLs with callbacks": function(){
+      console.log("1")
       var linkSpy = sinon.spy(),
           href = "/blah",
-          routes = {router: {
-            routeTo: function(url){
-              routes[url].call({})
-            }
-          }}
+          routes = {},
+          router = _.clone(this.router)
+      _.extend(router, {
+        routeTo: function(url){
+          routes[url].call({})
+        }
+      })
       this.$link.attr("href", href)
       routes[href] = linkSpy
       this.$container.bindNavigation({ buttonSelector: "a",
                                        routes: routes,
-                                       router: this.router })
+                                       router: router })
       this.$link.click()
       linkSpy.calledOnce.should.be.true
+    },
+    "sets active navigation item when clicked": function(){
+      var routes = {}
+      this.$link.wrap("<div class='navigation-item'>")
+      this.$container.bindNavigation({ buttonSelector: "a", routes: routes, router: this.router })
+      this.$link.click()
+      this.$link.parents(".navigation-item").hasClass("is-active").should.be.true
+    },
+    "unsets previously active navigation item": function(){
+      var $link2 = $("<a class='nav-button' href='#'></a>")
+      $link2.appendTo(this.$container)
+      this.$link.wrap("<div class='navigation-item is-active'>")
+      $link2.wrap("<div class='navigation-item is-active'>")
+      this.$container.bindNavigation({ buttonSelector: "a", router: this.router })
+      $link2.click()
+      this.$link.parents(".navigation-item").hasClass("is-active").should.be.false
     }
   }
 }
